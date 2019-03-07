1febf588980bfae0f5b7ef73cab96354ce27c15a
Niopsa <45314677+Niopsa@users.noreply.github.com>
2019-03-07 23:14:00 +0100

    Fin DM2

--- Bibliotheque.java
+++ Bibliotheque.java
@@ -2,65 +2,70 @@ import java.util.*;
 
 public class Bibliotheque{
 	private ArrayList<Document> document;
+	private ArrayList<Livre> livre;
 	private int capacite;
-	private int enCours = 0;
-	
+	private int NbLivre = 0;
+
 	public Bibliotheque(int capacite) {
 		this.capacite = capacite;
 		document = new ArrayList<Document>();
+		livre = new ArrayList<Livre>();
 	}
-	
+
 	public boolean ajouter(Document doc) {
-		if( enCours < capacite ) {
+		if( document.size() < capacite ) {
 			document.add(doc);
-			enCours ++;
 			return true;
 		}
 		else {
 			return false;
 		}
 	}
-	
+
 	public void afficherDocuments() {
 		for(Document doc : document) {
 			System.out.println(doc.getTitle() + " n°" + doc.getNumero());
 		}
 	}
-	
+
 	public void afficherAuteurs() {
-		for(Document doc : document) {
-			if(doc instanceof Livre) {
-				System.out.println(doc.getTitle() + " n°" + doc.getNumero() + " : " + ((Livre)doc).getAuthor());
-			}
+		for(Livre doc : livre) {
+			System.out.println(doc.getTitle() + " n°" + doc.getNumero() + " : " + doc.getAuthor());
 		}
 	}
-	
-	public void ajouterLivre(ArrayList<Document> doc) {
+
+	public void ajouterLivres(ArrayList<Document> doc) {
 		for(Document livre : doc) {
 			if(livre instanceof Livre) {
 				if(document.size() < capacite) {
 					document.add(livre);
+					NbLivre ++;
 				}
 			}
 		}
 	}
-	
+
+	public void ajouterLivre(Livre livre){
+		this.livre.add(livre);
+	}
+
 	public static void main(String[] argc) {
-		
+
 		Bibliotheque biblio = new Bibliotheque(50);
-		
-		
+		Livrotheque livro = new Livrotheque(25);
+
 		Roman neogicia = new Roman("neogicia", 1, "F. Fournier", 400, 0);
 		Manuel man = new Manuel("man 1", 2, "Auteur 1", 25, 3);
 		Revue rev1 = new Revue("rev 1", 3, 2019, 03);
-		
-		biblio.ajouter(neogicia);
-		biblio.ajouter(man);
+
+
+		livro.ajouterLivre(neogicia);
+		livro.ajouterLivre(man);
 		biblio.ajouter(rev1);
-		
+
 		biblio.afficherDocuments();
-		
-		
+		livro.afficherAuteurs();
+
 	}
-	
+
 }
---
+++ Livrotheque.java
@@ -0,0 +1,16 @@
+
+
+public class Livrotheque extends Bibliotheque{
+
+  public Livrotheque(int capacite){
+    super(capacite);
+  }
+
+  public void ajouterLivre(Livre livre){
+    super.ajouterLivre(livre);
+  }
+
+  public void afficherAuteurs(){
+    super.afficherAuteurs();
+  }
+}
